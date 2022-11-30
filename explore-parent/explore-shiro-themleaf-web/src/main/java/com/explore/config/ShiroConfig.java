78a5ecb953822ee977b1fd73f8dcb02009839ccd
wwang-luse <wwangtaoc11@163.com>
2022-11-30 13:26:18 +0800

    shiro

--- explore-parent/explore-shiro-themleaf-web/src/main/java/com/explore/config/ShiroConfig.java
+++ explore-parent/explore-shiro-themleaf-web/src/main/java/com/explore/config/ShiroConfig.java
@@ -1,62 +1,55 @@
 package com.explore.config;
 
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+/*import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SessionsSecurityManager;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;*/
 
 import com.explore.online.realm.MyRealm;
 
-@Configuration
+//@Configuration
 public class ShiroConfig {
-/**
- * 1.Realm 代表系统资源	
- * 
- */
-@Bean
-public MyRealm  myReaml(HashedCredentialsMatcher credentialsMatcher) {
+//1.Realm 代表系统资源	
+//@Bean
+/*public MyRealm  myReaml(HashedCredentialsMatcher credentialsMatcher) {
 	MyRealm myRealm = new MyRealm();
-	myRealm.setCredentialsMatcher(credentialsMatcher);
+	///myRealm.setCredentialsMatcher(credentialsMatcher);
 	return myRealm;
-}
-/**
- * 2.SecurityManager 流程控制
- * @param myRealm
- * @return
- */
-@Bean
-public DefaultWebSecurityManager securityManager(MyRealm myRealm) {
+}*/
+
+//2.SecurityManager 流程控制
+//@Bean
+/*public SessionsSecurityManager securityManager(MyRealm myRealm) {
 	//创建SecurityManager对象
 	DefaultWebSecurityManager security = new DefaultWebSecurityManager();
 	//将Realm存入到SecurityManager中
-	security.setRealm(myRealm);
+	//security.setRealm(myRealm);
 	
 	return security;
-}
-/**
- *3. 请求过滤
- * @param securityManager
- * @return
- */
-@Bean
-public ShiroFilterChainDefinition shiroFilterFactoryBean () {
-	 DefaultShiroFilterChainDefinition definition = new DefaultShiroFilterChainDefinition();
-	return definition;
-}
+}*/
+
+//3. 请求过滤
+//@Bean
+/*public ShiroFilterChainDefinition  shiroFilter() {
+	DefaultShiroFilterChainDefinition   shiroFilter = new DefaultShiroFilterChainDefinition ();
+
+	return shiroFilter;
+}*/
 /**
  * 告诉Shiro使用什么方式加密
  * @return
  */
-@Bean
-public HashedCredentialsMatcher credentialsMatcher() {
+///@Bean
+/*public HashedCredentialsMatcher credentialsMatcher() {
 	HashedCredentialsMatcher  matcher= new HashedCredentialsMatcher();
-	matcher.setHashAlgorithmName("sha512");//加密方式
-	matcher.setHashIterations(8192);//迭代加密次数
+	matcher.setHashAlgorithmName("Md5");//加密方式
+	matcher.setHashIterations(1024);//迭代加密次数
 	return matcher;
+	*/
 	
-	
-}
+//}
 
 }
--- explore-parent/explore-shiro-themleaf-web/src/main/java/com/explore/online/controller/MemberController.java
+++ explore-parent/explore-shiro-themleaf-web/src/main/java/com/explore/online/controller/MemberController.java
@@ -2,20 +2,22 @@ package com.explore.online.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.shiro.SecurityUtils;
+/*import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.ExcessiveAttemptsException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.Subject;*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 import com.explore.common.resp.ResponseMessage;
@@ -34,36 +36,26 @@ public String login() {
  * @param request
  * @return
  */
-public ResponseMessage login(@RequestBody JSONObject jsonObject,HttpServletRequest request) {
-	ResponseMessage responseMessage = ResponseMessage.success();
-	try {
-		String username = jsonObject.getString("username");
-		String password = jsonObject.getString("password");
-		//1.初始化获取securityManager并获取Subject对象
-		Subject currentUser  = SecurityUtils.getSubject();
-		//2.创建token对象，web应用用户名和密码从页面传递
-		UsernamePasswordToken  token = new UsernamePasswordToken(username, password.toCharArray());
-		//3.完成登录
-		currentUser.login(token);
-	} catch (UnknownAccountException e) {
-		// TODO: handle exception
-		logger.error(e.getMessage());
-	} catch (IncorrectCredentialsException  e) {
-	// TODO: handle exception
-		logger.error(e.getMessage());
-	} catch (LockedAccountException  e) {
-	// TODO: handle exception
-		logger.error(e.getMessage());
-	} catch (ExcessiveAttemptsException e) {
-	// TODO: handle exception
-		logger.error(e.getMessage());
-	}catch (AuthenticationException e) {
-	// TODO: handle exception
-		logger.error(e.getMessage());
-	}catch (Exception e) {
-	// TODO: handle exception
-		logger.error(e.getMessage());
-	}
-	return responseMessage;
-}
+	/*
+	 * @PostMapping("/login")
+	 * 
+	 * @ResponseBody public ResponseMessage login(@RequestBody JSONObject
+	 * jsonObject,HttpServletRequest request) { ResponseMessage responseMessage =
+	 * ResponseMessage.success(); try { String username =
+	 * jsonObject.getString("username"); String password =
+	 * jsonObject.getString("password"); //1.初始化获取securityManager并获取Subject对象
+	 * Subject currentUser = SecurityUtils.getSubject();
+	 * //2.创建token对象，web应用用户名和密码从页面传递 UsernamePasswordToken token = new
+	 * UsernamePasswordToken(username, password.toCharArray()); //3.完成登录
+	 * currentUser.login(token); } catch (UnknownAccountException e) { // TODO:
+	 * handle exception logger.error(e.getMessage()); } catch
+	 * (IncorrectCredentialsException e) { // TODO: handle exception
+	 * logger.error(e.getMessage()); } catch (LockedAccountException e) { // TODO:
+	 * handle exception logger.error(e.getMessage()); } catch
+	 * (ExcessiveAttemptsException e) { // TODO: handle exception
+	 * logger.error(e.getMessage()); }catch (AuthenticationException e) { // TODO:
+	 * handle exception logger.error(e.getMessage()); }catch (Exception e) { //
+	 * TODO: handle exception logger.error(e.getMessage()); } return
+	 * responseMessage; }
+	 */
 }
